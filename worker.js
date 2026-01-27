eaaa6bfd076634435049c1eef9a834de682dedba
niconanashi <79348397+niconanashi@users.noreply.github.com>
2026-01-27 22:06:57 +0900

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,1 @@
-self.onmessage = async (e) => {
-  const { id, type, data } = e.data;
-  if (type === 'LOAD') {
-    try {
-      importScripts(data.coreURL);
-      const ffmpeg = await self.createFFmpegCore({
-        mainScriptUrlOrBlob: `${data.coreURL}#${btoa(JSON.stringify({ wasmURL: data.wasmURL, workerURL: data.workerURL }))}`
-      });
-      self.ffmpeg = ffmpeg;
-      self.postMessage({ id, type, data: true });
-    } catch (err) {
-      self.postMessage({ id, type: 'ERROR', data: err.toString() });
-    }
-  }
-};
+importScripts("https://unpkg.com/@ffmpeg/ffmpeg@0.12.7/dist/umd/ffmpeg.js");
