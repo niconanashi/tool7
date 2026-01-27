c94f4cfebfd9129d5a4d39648a973fdb765be2d9
niconanashi <79348397+niconanashi@users.noreply.github.com>
2026-01-27 21:53:24 +0900

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,4 @@
+importScripts("https://unpkg.com/@ffmpeg/ffmpeg@0.12.7/dist/umd/ffmpeg.js");
+self.onmessage = async (e) => {
+};
+import("https://unpkg.com/@ffmpeg/ffmpeg@0.12.7/dist/esm/worker.js");
